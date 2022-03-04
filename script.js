795de146517d7f8261a02d11cc00569b95a54f49
SakkyOP <saksham298@gmail.com>
2022-03-04 15:13:02 +0530

    added audio

--- script.js
+++ script.js
@@ -17,9 +17,15 @@ function randrange(min, max){
     return Math.floor((Math.random()*(max-min+1))+min);
 }
 
+function playsound(id){
+    var audio = new Audio("audio/"+id+".mp3");
+    audio.play();
+}
+
 function activate(id){
     $("#"+id).fadeOut(300);
     $("#"+id).fadeIn(300);
+    playsound(id);
 }
 
 function nextSequence(){
@@ -30,6 +36,8 @@ function nextSequence(){
 }
 
 function gameover(){
+    var audio = new Audio("audio/wrong.mp3");
+    audio.play();
     gameloop = false;
     sequence = [];
     ans_seq = [];
@@ -43,6 +51,7 @@ function answer(id){
     ans_seq.push(parseInt(id));
     if (sequence.length === ans_seq.length) {
         if (checkLists(sequence,ans_seq)){
+            playsound(id);
             ans_seq = [];
             ans_index = 0;
             lvl++;
@@ -52,13 +61,12 @@ function answer(id){
         }
 
     } else {
-        console.log(sequence);
-        console.log(ans_seq);
-        console.log(ans_index);
         if (sequence[ans_index] != ans_seq[ans_index]) {
             gameover();
+        } else {
+            playsound(id);
+            ans_index++;
         }
-        ans_index++;
     }
 }
 
